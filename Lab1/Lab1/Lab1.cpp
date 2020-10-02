d196bbdf15466638ba680d345bf8bb61e84f4815
Konstantin <ryabov.kostantin18@gmail.com>
2020-10-02 23:50:06 +0300

    constants usage add

--- Lab1/Lab1/Lab1.cpp
+++ Lab1/Lab1/Lab1.cpp
@@ -6,36 +6,36 @@ int main() {
 
     std::cout << "1.\nRyabov Konstantin Dmitrievich, 201-351" << std::endl;
 
-    std::cout << "2.\n" << "min int=" << std::numeric_limits<int>::min() << ','
-        << " max int=" << std::numeric_limits<int>::max() << ','
+    std::cout << "2.\n" << "min int=" << INT_MIN << ','
+        << " max int=" << INT_MAX << ','
         << " size of int=" << sizeof(int) << std::endl
 
-        << "min unsigned int=" << std::numeric_limits<unsigned int>::min() << ','
-        << " max unsigned int=" << std::numeric_limits<unsigned int>::max() << ','
+        << "min unsigned int=" << 0 << ','
+        << " max unsigned int=" << UINT_MAX << ','
         << " size of unsigned int=" << sizeof(unsigned int) << std::endl
 
-        << "min short=" << std::numeric_limits<short>::min() << ','
-        << " max short=" << std::numeric_limits<short>::max() << ','
+        << "min short=" << SHRT_MIN << ','
+        << " max short=" << SHRT_MAX << ','
         << " size of short" << sizeof(short) << std::endl
 
-        << "min unsigned short=" << std::numeric_limits<unsigned short>::min() << ','
-        << " max unsigned short=" << std::numeric_limits<unsigned short>::max() << ','
+        << "min unsigned short=" << 0 << ','
+        << " max unsigned short=" << USHRT_MAX << ','
         << "size of unsigned short=" << sizeof(unsigned short) << std::endl
 
-        << "min long=" << std::numeric_limits<long>::min() << ','
-        << " max long=" << std::numeric_limits<long>::max() << ','
+        << "min long=" << LONG_MIN << ','
+        << " max long=" << LONG_MAX << ','
         << " size of long=" << sizeof(long) << std::endl
 
-        << "min long long=" << std::numeric_limits<long long>::min() << ','
-        << " max long long=" << std::numeric_limits<long long>::max() << ','
+        << "min long long=" << LLONG_MIN << ','
+        << " max long long=" << LLONG_MAX << ','
         << " size of long long=" << sizeof(long long) << std::endl
 
         << "min double=" << std::numeric_limits<double>::min() << ','
         << " max double=" << std::numeric_limits<double>::max() << ','
         << " size of double=" << sizeof(double) << std::endl
 
-        << "min char=" << static_cast<int>(std::numeric_limits<char>::min()) << ','
-        << " max char=" << static_cast<int>(std::numeric_limits<char>::max()) << ','
+        << "min char=" << CHAR_MIN << ','
+        << " max char=" << CHAR_MAX << ','
         << " size of char=" << sizeof(char) << std::endl
 
         << "min bool=" << std::numeric_limits<bool>::min() << ','
@@ -56,17 +56,17 @@ int main() {
     std::cout << "char " << static_cast<char>(n) << std::endl;
 
     std::cout << "4.\nВведите коэффициенты a * x = b: ";
-    int a, b;
+    float a, b;
     std::cin >> a >> b;
 
     std::cout << "x = " << b << '/' << a << std::endl;
-    std::cout << "x = " << double(b) / double(a) << std::endl;
-    std::cout << "Ответ: " << double(b) / double(a) << std::endl;
+    std::cout << "x = " << b/a << std::endl;
+    std::cout << "Ответ: " << b/a << std::endl;
 
     std::cout << "5.\nВведите координаты отрезка на прямой: ";
-    int x1, x2;
+    float x1, x2;
     std::cin >> x1 >> x2;
-    std::cout << "Середина отрезка находится в точке с координатой " << (x2 + x1) / 2. << std::endl;
+    std::cout << "Середина отрезка находится в точке с координатой " << (x2 + x1) / 2 << std::endl;
 
     return 0;
 }
